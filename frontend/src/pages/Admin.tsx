74009f303525eaffa3201a29964955f14a9bb2f2
VaibhavBansal26 <vaibhav.bansal945@gmail.com>
2025-03-29 14:28:12 -0400

    test

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { MdDelete } from "react-icons/md";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_ADMIN_URL;
 const API_ADMIN_PORT = import.meta.env.VITE_API_ADMIN_PORT;
 
 const server = `${API_URL}:${API_ADMIN_PORT}`;
